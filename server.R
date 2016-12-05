2a2d1a853b27f4ef36f55e4b24199e01c31df8fb
baylesa <andrewbayles1@gmail.com>
2016-12-04 19:09:32 -0800

    interactive map and responds to inputs

--- server.R
+++ server.R
@@ -6,13 +6,14 @@ library(leaflet)
 
 #Read in data and change some factors to character
 college.data <- read.csv("data/data.csv", header = TRUE)
+View(college.data)
 college.data$STABBR <- as.character(college.data$STABBR)
 college.data$INSTNM <- as.character(college.data$INSTNM)
 
 computeDist <- function(college, data) {
   temp <- data[data[,2]==college[,1] & data[,3]==college[,2],]
   temp$dist <- apply(temp[,4:6],1,function(x) sqrt(sum((x-college[,3:5])^2)))
-  return(temp[order(temp$dist),c(-3,-11)])
+  return(temp[order(temp$dist),c(-3,-17)])
 }
 
 shinyServer(function(input, output) {
@@ -56,9 +57,14 @@ shinyServer(function(input, output) {
   })
   
   output$mymap <- renderLeaflet({
-    leaflet(college.data) %>% addCircles(lng = ~LONGITUDE, lat = ~LATITUDE)%>%
+    df <- head(computeDist(data.frame(STABBR = stateInput(), PREDDEG = degreeInput(), UGDS = ugdsInput(), SAT_AVG = satInput(), ACTCMMID = actInput())
+                           , college.data[,c(4,3,7,9,11,12,10,13,14,15,16,17)]), n =3)
+    leaflet(df) %>% addMarkers(lng = ~LONGITUDE, lat = ~LATITUDE, popup = 
+                                           paste0(df$INSTNM, "<br>", "Location: ", df$CITY, ",",df$STABBR , "<br>", "# of Undergrads: ", df$UGDS, "<br>", "Admission Rate: ", df$ADM_RATE ))%>%
       addProviderTiles("Stamen.TonerLite",
                        options = providerTileOptions(noWrap = TRUE)
       ) 
   })
 })
+
+
