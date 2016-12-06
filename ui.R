77e1cfb6bd705096f043e28c5aa3452374ca64cf
baylesa <andrewbayles1@gmail.com>
2016-12-05 21:13:06 -0800

    tabs added, need to fix input variable names

--- ui.R
+++ ui.R
@@ -2,42 +2,75 @@ library(shiny)
 library(leaflet)
 
 ui <- bootstrapPage(# Use a fluid Bootstrap layout
-  fluidPage(    
+  navbarPage("US College Scorecard 2014-2015",
+      #Tab panel for table output
+      tabPanel("Table",    
+          # Generate a row with a sidebar
+          sidebarLayout(      
+            
+            # Define the sidebar with one input
+            sidebarPanel(
+              #numericInput('ugds', 'Undergraduate Body Size', 1,
+              #            min = 1, max = 100000),
+              sliderInput("ugds", 'Undergraduate Body Size', min = 0, 
+                          max = 61470, value = c(0, 61470), step = 10000),
+              sliderInput('sat', 'SAT score',
+                          min = 0, max = 1600, value = c(0, 1600)),
+              sliderInput('act', 'ACT score',
+                          min = 1, max = 36, value = c(1, 36)),
+              selectInput("degree.type", "Degree Type:", 
+                          choices = list("All" = 0, "Two-year" = 2,"Four-year" = 3),
+                          selected = 0),
+              selectInput("publicOrPrivate", "Public or Private School", 
+                          choices = list("All" = 0, "Public" = 1, "Private Non-Profit" = 2, "Private For-Profit" = 3), 
+                          selected = 0),
+              uiOutput("stateSelector"),
+              actionButton("submitCollege", "Find Colleges"),
+              helpText("Data from the famous (Fisher's or Anderson's) iris data set")
+            ),
+            
+            # Create a spot for the barplot
+            mainPanel(
+              #verbatimTextOutput("summary"),
+              tableOutput("summary")
+              
+            )
+          )
     
-    # Give the page a title
-    titlePanel("US College Scorecard 2014-2015"),
-    
-    # Generate a row with a sidebar
-    sidebarLayout(      
-      
-      # Define the sidebar with one input
-      sidebarPanel(
-        #numericInput('ugds', 'Undergraduate Body Size', 1,
-         #            min = 1, max = 100000),
-        sliderInput("ugds", 'Undergraduate Body Size', min = 0, 
-                    max = 61470, value = c(0, 61470), step = 10000),
-        sliderInput('sat', 'SAT score',
-                     min = 0, max = 1600, value = c(0, 1600)),
-        sliderInput('act', 'ACT score',
-                     min = 1, max = 36, value = c(1, 36)),
-        selectInput("degree.type", "Degree Type:", 
-                    choices = list("All" = 0, "Two-year" = 2,"Four-year" = 3),
-                    selected = 0),
-        selectInput("publicOrPrivate", "Public or Private School", 
-                    choices = list("All" = 0, "Public" = 1, "Private Non-Profit" = 2, "Private For-Profit" = 3), 
-                    selected = 0),
-        uiOutput("stateSelector"),
-        actionButton("submitCollege", "Find Colleges"),
-        helpText("Data from the famous (Fisher's or Anderson's) iris data set")
+   
       ),
-      
-      # Create a spot for the barplot
-      mainPanel(
-        #verbatimTextOutput("summary"),
-        leafletOutput("mymap"),
-        tableOutput("summary")
-        
+      tabPanel("Map",
+               # Generate a row with a sidebar
+               sidebarLayout(      
+                 
+                 # Define the sidebar with one input
+                 sidebarPanel(
+                   #numericInput('ugds', 'Undergraduate Body Size', 1,
+                   #            min = 1, max = 100000),
+                   sliderInput("ugds", 'Undergraduate Body Size', min = 0, 
+                               max = 61470, value = c(0, 61470), step = 10000),
+                   sliderInput('sat', 'SAT score',
+                               min = 0, max = 1600, value = c(0, 1600)),
+                   sliderInput('act', 'ACT score',
+                               min = 1, max = 36, value = c(1, 36)),
+                   selectInput("degree.type", "Degree Type:", 
+                               choices = list("All" = 0, "Two-year" = 2,"Four-year" = 3),
+                               selected = 0),
+                   selectInput("publicOrPrivate", "Public or Private School", 
+                               choices = list("All" = 0, "Public" = 1, "Private Non-Profit" = 2, "Private For-Profit" = 3), 
+                               selected = 0),
+                   uiOutput("stateSelector"),
+                   actionButton("submitCollege", "Find Colleges"),
+                   helpText("Data from the famous (Fisher's or Anderson's) iris data set")
+                 ),
+                 
+                 # Create a spot for the barplot
+                 mainPanel(
+                   #verbatimTextOutput("summary"),
+                   leafletOutput("mymap")
+                   
+                 )
+               )
       )
-      
     )
-))
+)
