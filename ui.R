9a27da8ca5fb925dd6ab5a8f9e3be4bc69e7a867
John Soter <john.soter@outlook.com>
2016-12-07 00:59:17 -0800

    added search feature

--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(leaflet)
+library(shinydashboard)
 
 ui <- bootstrapPage(# Use a fluid Bootstrap layout
   navbarPage("US College Scorecard 2014-2015",
@@ -10,6 +11,7 @@ ui <- bootstrapPage(# Use a fluid Bootstrap layout
             
             # Define the sidebar with one input
             sidebarPanel(
+              
               #numericInput('ugds', 'Undergraduate Body Size', 1,
               #            min = 1, max = 100000),
               sliderInput("ugds", 'Undergraduate Body Size', min = 0, 
@@ -42,6 +44,8 @@ ui <- bootstrapPage(# Use a fluid Bootstrap layout
                  
                  # Define the sidebar with one input
                  sidebarPanel(
+                   sidebarSearchForm('school', 'button', label = "Search...",
+                                     icon = shiny::icon("search")),
                    #numericInput('ugds', 'Undergraduate Body Size', 1,
                    #            min = 1, max = 100000),
                    sliderInput("ugds.map", 'Undergraduate Body Size', min = 0, 
@@ -64,6 +68,7 @@ ui <- bootstrapPage(# Use a fluid Bootstrap layout
                  mainPanel(
                    #verbatimTextOutput("summary"),
                    leafletOutput("mymap")
+                   
                    
                  )
                )
