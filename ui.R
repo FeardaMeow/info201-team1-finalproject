1d2bf6efbfd66d22f958712a6dce7f7ec90c7e26
John Soter <john.soter@outlook.com>
2016-12-07 15:21:31 -0800

    added fixed serach button, cleaner display, and ability to change marker colors

--- ui.R
+++ ui.R
@@ -44,8 +44,7 @@ ui <- bootstrapPage(# Use a fluid Bootstrap layout
                  
                  # Define the sidebar with one input
                  sidebarPanel(
-                   sidebarSearchForm('school', 'button', label = "Search...",
-                                     icon = shiny::icon("search")),
+                   textInput("school", label = '',value = "School Name..."),
                    #numericInput('ugds', 'Undergraduate Body Size', 1,
                    #            min = 1, max = 100000),
                    sliderInput("ugds.map", 'Undergraduate Body Size', min = 0, 
